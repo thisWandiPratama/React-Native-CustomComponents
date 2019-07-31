7df83480cc91fb4350d4e3d9266cc91cb0e1dffd
OneBlog <wandi99pratama@gmail.com>
2019-07-31 17:22:32 +0700

    Upload Costom Component React Native

---
+++ App.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import MainApp from './src/MainApp';
+AppRegistry.registerComponent('CustomComponents', () => MainApp);
---
+++ src/Button/index.js
@@ -0,0 +1,62 @@
+import React, { Component, PropTypes } from 'react';
+import {
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import Base, { Default, Danger, Info, Success } from './styles';
+
+const { array, string, object, bool, func, any } = PropTypes;
+class Button extends Component {
+    static propTypes = {
+        // Defined on step 4
+        hildren: any,
+        danger: bool,
+        info: bool,
+        style: View.propTypes.style,
+        success: bool,
+        onPress: func,
+    };
+    getTheme() {
+        // Defined on step 5
+        const { danger, info, success } = this.props;
+        if (info) {
+            return Info;
+        }
+        if (success) {
+            return Success;
+        }
+        if (danger) {
+            return Danger;
+        }
+        return Default;
+    }
+
+
+    render() {
+        const theme = this.getTheme();
+        const {
+            children,
+            onPress,
+            style,
+            rounded,
+        } = this.props;
+
+        return (
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={[
+                    Base.main,
+                    theme.main,
+                    rounded ? Base.rounded : null,
+                    style,
+                ]}
+                onPress={onPress}
+            >
+                <Text style={[Base.label, theme.label]}>{children}</Text>
+            </TouchableOpacity>
+        );
+    }
+}
+
+export default Button; // Don't forget to export it!
